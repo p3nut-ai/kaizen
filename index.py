3763012600257da900bc883a52bea6b7d356766a
p3nut-ai <jaquiasjonathan2@gmail.com>
2024-11-29 07:00:38 +0800

    finished HTML and css

--- index.py
+++ index.py
@@ -1,12 +1,30 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+# Declare the specific GitHub username
+USERNAME = 'kaizensol'  #GitHub username
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # GitHub API URL to fetch repositories for the specified username
+    url = f'https://api.github.com/users/{USERNAME}/repos'
+    headers = {
+        "Authorization" : "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"
 
-if __name__ == "__main__":
-    pass
+    }
+    # Make a GET request to the GitHub API
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        repos = response.json()  # Parse the JSON response
+        print(repos)
+        return render_template('index.html', repos=repos, username=USERNAME)
+    else:
+        print('Error')
+        return render_template('index.html', repos=[], username=USERNAME, error='User  not found or an error occurred.')
+
+if __name__ == '__main__':
+    app.run(debug=True)
